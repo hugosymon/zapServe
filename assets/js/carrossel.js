5ebb04eda24ed0c17dd6d73725cddc195bf36e7b
Thiago Silva <smt.thiago99@gmail.com>
2025-01-09 15:19:32 -0300

    feat: add carroussel

---
+++ assets/js/carrossel.js
@@ -0,0 +1,39 @@
+const carrosselItens = document.querySelector(".carrossel-itens");
+const btnPrev = document.querySelector(".btn-prev");
+const btnNext = document.querySelector(".btn-next");
+const indicadores = document.querySelector(".indicadores");
+
+const itens = document.querySelectorAll(".item-carrossel");
+let index = 0;
+
+// Criar indicadores
+itens.forEach((_, i) => {
+  const span = document.createElement("span");
+  span.dataset.index = i;
+  if (i === 0) span.classList.add("active");
+  indicadores.appendChild(span);
+});
+
+const updateIndicadores = () => {
+  document.querySelectorAll(".indicadores span").forEach((el, i) => {
+    el.classList.toggle("active", i === index);
+  });
+};
+
+const moveToIndex = (newIndex) => {
+  if (newIndex < 0) index = itens.length - 1;
+  else if (newIndex >= itens.length) index = 0;
+  else index = newIndex;
+  carrosselItens.style.transform = `translateX(-${index * 100}%)`;
+  updateIndicadores();
+};
+
+btnPrev.addEventListener("click", () => moveToIndex(index - 1));
+btnNext.addEventListener("click", () => moveToIndex(index + 1));
+
+document.querySelectorAll(".indicadores span").forEach((span) => {
+  span.addEventListener("click", () => moveToIndex(Number(span.dataset.index)));
+});
+
+// Navegação automática
+setInterval(() => moveToIndex(index + 1), 5000);
